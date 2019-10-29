b42e81a63d0efe0918489fdc5068605ba4e91981
Your Name <you@example.com>
2019-10-29 22:20:30 +0800

    [chull]增加了waiter详情页面的返回功能

--- src/pages/waiter/Details.vue
+++ src/pages/waiter/Details.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="detail">
+    <!-- 按钮 -->
+    <div>
+      <el-button size="small" type="primary" @click="returnHandler">返回</el-button>
+    </div>
+    <!-- /按钮 -->
     员工详情页面
   </div>
 </template>
 <script>
-
+export default {
+  methods: {
+    returnHandler() {
+      // 返回员工页面
+      // this.$router.push("/waiter/Waiter")
+      this.$router.go(-1)
+    }
+  }
+}
 </script>
 <style scoped>
 
