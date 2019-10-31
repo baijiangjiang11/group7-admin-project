72f8fdf0b6d4412b054d7ff5dc93f4fab8a9bf52
ll-wy <1941054259@qq.com>
2019-10-31 11:37:15 +0800

    lwy

---
+++ src/pages/category/Details.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="categoryDetails">
+    <el-button size="small" type="text" @click="backHandler">返回</el-button>
+
+  </div>
+</template>
+<script>
+export default {
+
+  created() {
+
+  },
+  methods: {
+    // 返回
+    backHandler() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,73 +1,65 @@
 import request from '@/utils/request'
+import { post, post_array } from '@/utils/request'
 
 export default {
-  namespaced:true,
-  state:{
-    categories:[],
-    visible:false,
-    title:"添加顾客信息"
+  namespaced: true,
+  state: {
+    categories: [],
+    loading: false,
+    visible: false,
+    title: '添加栏目信息'
+
   },
-  getters:{
-    categorySize(state){
-      return state.categories.length;
+  getters: {
+
+  },
+  mutations: {
+    showModal(state) {
+      state.visible = true
     },
-    orderCategory:(state)=>{
-      return function(flag){
-        state.categories.sort((a,b)=>{
-          if(a[flag] > b[flag]){
-            return -1;
-          } else {
-            return 1;
-          }
-        })
-        return state.categories;
-      }
+    closeModal(state) {
+      state.visible = false
+    },
+    setTitle(state, title) {
+      state.title = title
+    },
+    // 刷新
+    refreshCategories(state, categories) {
+      state.categories = categories
+    },
+    beginLoading(state) {
+      state.loading = true
+    },
+    endLoading(state) {
+      state.loading = false
     }
+
   },
-  mutations:{
-    showModal(state){
-      state.visible = true;
+  actions: {
+    // 渲染栏目信息
+    async findAllCategories({ dispatch, commit }) {
+      commit('beginLoading')
+      const response = await request.get('/category/findAll')
+      commit('refreshCategories', response.data)
+      setTimeout(() => {
+        commit('endLoading')
+      }, 1000)
     },
-    closeModal(state){
-      state.visible = false;
+    // 根据id删除栏目信息
+    async deleteCategoryById(context, id) {
+      const response = await request.get('/category/deleteById?id=' + id)
+      context.dispatch('findAllCategories')
+      return response
     },
-    refreshCategories(state,categories){
-      state.categories = categories;
-    },
-    setTitle(state,title){
-      state.title = title;
-    }
-  },
-  actions:{
-    async batchDeleteCategory(context,ids){
-      // 1. 批量删除
-      let response = await request.post("/category/batchDelete",{ids})
-      // 2. 分发
-      context.dispatch("findAllCategories");
-      // 3. 返回结果
-      return response;
-    },
-    async deleteCategoryById(context,id){
-      let response = await request.get("/category/deleteById?id="+id);
-      context.dispatch("findAllCategories");
-      return response;
-    },
-    async findAllCategories(context){
-      // 1. ajax查询
-      let response = await request.get("/category/findAll");
-      // 2. 将查询结果更新到state中
-      context.commit("refreshCategories",response.data);
-    },
-    // payload 顾客信息
-    async saveOrUpdateCategory({commit,dispatch},payload){
-      // 1. 保存或更新
-      let response = await request.post("/category/saveOrUpdate",payload)
-      // 2. 刷新页面
-      dispatch("findAllCategories");
-      // 3. 关闭模态框
-      commit("closeModal");
-      // 4. 提示
-      return response;
+    // 更新栏目信息
+    async saveOrUpdateCategory({ commit, dispatch }, payload) {
+      // 更新数据
+      const response = await post('/category/saveOrUpdate', payload)
+      // 刷新页面
+      dispatch('findAllCategories')
+      // 关闭模态框
+      commit('closeModal')
+      return response
     }
   }
 }
